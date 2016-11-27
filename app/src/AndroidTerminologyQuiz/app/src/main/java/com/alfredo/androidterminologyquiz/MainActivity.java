1d5517880e91af6cdf97e66693428f5d59e93183
AlfredoMarrero <a.e.marrero@hotmail.com>
2016-11-27 11:32:55 -0500

    Add functionality for multiple choice and short answer.

--- app/src/AndroidTerminologyQuiz/app/src/main/java/com/alfredo/androidterminologyquiz/MainActivity.java
+++ app/src/AndroidTerminologyQuiz/app/src/main/java/com/alfredo/androidterminologyquiz/MainActivity.java
@@ -1,20 +1,29 @@
 package com.alfredo.androidterminologyquiz;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView score;
     TextView questionTextView;
     CheckBox multipleChoice, shortAnswer;
     LinearLayout checkBoxLayout;
     QuestionSet q = new QuestionSet();
+    //List <Integer> checkboxesSelected;
+    List<Integer> checkboxesSelected = new ArrayList<>();
+    double userScore = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,13 +38,20 @@ public class MainActivity extends AppCompatActivity {
 
         multipleChoice.setChecked(true);
         setCheckBoxQuestions();
+
+        score = (TextView) findViewById(R.id.user_score);
+        score.setText(userScore + "/" + q.getTotalNumberOfQuestions() + ".0");
     }
+
     //Display short answer questions
     public void displayShortAnswer(View view){
 
         if(shortAnswer.isChecked()){
             multipleChoice.setChecked(false);
-            setShortAnswerQuestion();
+            //setShortAnswerQuestion();
+            q.setShortAnswerQuestions();
+            //setCheckBoxQuestions();
+            setShortAnswerQuestions();
         }
     }
     //Displays multiple choice questions
@@ -47,15 +63,47 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void nextQuestion(View view){
-        setCheckBoxQuestions();
+        int numberOfCheckboxesChecked = 0;
+
+      //  for (int i = 0; i < q.maxNumCheckbox(); i++){
+            //CheckBox findIfChecked = (CheckBox) findViewById(R.id.(i));
+            //ViewGroup viewGroup = (ViewGroup) ((ViewGroup) this.findViewById(android.R.id.)).getChildAt(0);
+        //}
+        int checkboxSelected;
+
+        for(int index=0; index< checkBoxLayout.getChildCount(); ++index) {
+            CheckBox nextChild = (CheckBox) checkBoxLayout.getChildAt(index);
+
+            if(nextChild.isChecked()){
+                checkboxesSelected.add(nextChild.getId());
+                //checkboxSelected = nextChild.getId();
+                numberOfCheckboxesChecked++;
+            }
+        }
+
+        if(numberOfCheckboxesChecked == 1 && !checkboxesSelected.isEmpty() && checkboxesSelected.get(0) == q.getAnswer()) {
+
+            setCheckBoxQuestions();
+            userScore++;
+            Context context = getApplicationContext();
+            CharSequence text = "You answer is correct!";
+            int duration = Toast.LENGTH_LONG;
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }
+        else{
+
+            userScore -= 0.5;
+        }
+        score.setText(userScore + "/" + q.getTotalNumberOfQuestions() + ".0");
+        checkboxesSelected.clear();
     }
 
     public void setCheckBoxQuestions(){
-        questionTextView.setText(q.getEasyQuestion());
 
+        questionTextView.setText(q.getEasyQuestion());
         ActionBar.LayoutParams layoutParams = new ActionBar.LayoutParams(ActionBar.LayoutParams.WRAP_CONTENT, ActionBar.LayoutParams.WRAP_CONTENT);
         q.setQuestions();
-
         checkBoxLayout.removeAllViewsInLayout();
 
         for (int i = 0; i < q.maxNumCheckbox(); i++){
@@ -63,17 +111,33 @@ public class MainActivity extends AppCompatActivity {
             CheckBox checkBox = new CheckBox(this);
             checkBox.setLayoutParams(layoutParams);
             checkBox.setText(q.getCheckboxText());
-            //checkBox.setId(Integer.parseInt(q.getCheckboxText()));
+            checkBox.setId(i);
             checkBoxLayout.addView(checkBox);
             q.nextCheckboxIndex();
         }
-
         q.nextIndex();
     }
 
-    public void setShortAnswerQuestion(){
+    public void setShortAnswerQuestions(){
+
+        questionTextView.setText(q.getEasyQuestion());
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ActionBar.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.WRAP_CONTENT);
+        layoutParams.setMargins(0, 15, 0, 0);
+        q.setQuestions();
+        checkBoxLayout.removeAllViewsInLayout();
+
+            EditText shortAnswer = new EditText(this);
+            //CheckBox checkBox = new CheckBox(this);
+            shortAnswer.setLayoutParams(layoutParams);
+//          shortAnswer.setText(q.getCheckboxText());
+            shortAnswer.setId(0);
+            checkBoxLayout.addView(shortAnswer);
+            q.nextCheckboxIndex();
+            q.nextIndex();
 
     }
+
+
 }
 
 
--- app/src/AndroidTerminologyQuiz/app/src/main/java/com/alfredo/androidterminologyquiz/QuestionSet.java
+++ app/src/AndroidTerminologyQuiz/app/src/main/java/com/alfredo/androidterminologyquiz/QuestionSet.java
@@ -62,42 +62,55 @@ public class QuestionSet
         questionsArray[3].answer = 2;
     }
 
-//    public void setMediumQuestions(){
-//
-//        questionsArray[0].questionHeader = "What are Intents and Intent Filters?";
-//        questionsArray[1].questionHeader = "What are the three fundamental cases of Intents?";
-//        questionsArray[2].questionHeader = "How many types of intents are there?";
-//        questionsArray[3].questionHeader = "What are the two types of intents?";
-//
-//        questionsArray[0].checkBox[0]  = "Question 1 answer";
-//        questionsArray[0].checkBox[1] = "Q 1 Answer 2";
-//        questionsArray[0].checkBox[2]  = "Q 1 answer 3";
-//        questionsArray[0].checkBox[3]  = "Q 1 answer 4";
-//        questionsArray[0].maxNumCheckbox = 4;
-//        questionsArray[0].answer = 2;
-//
-//        questionsArray[1].checkBox[0]  = "Question 2 answer";
-//        questionsArray[1].checkBox[1] = "Q 2 Answer 2";
-//        questionsArray[1].checkBox[2]  = "Q 2 answer 3";
-//        questionsArray[1].checkBox[3]  = "Q 2 answer 4";
-//        questionsArray[1].maxNumCheckbox = 4;
-//        questionsArray[1].answer = 2;
-//
-//        questionsArray[2].checkBox[0]  = "Question 3 answer";
-//        questionsArray[2].checkBox[1] = "Q 3 Answer 2";
-//        questionsArray[2].checkBox[2]  = "Q 3 answer 3";
-//        questionsArray[2].checkBox[3]  = "Q 3 answer 4";
-//        questionsArray[2].maxNumCheckbox = 4;
-//        questionsArray[2].answer = 2;
-//
-//        questionsArray[3].checkBox[0]  = "Question 4 answer";
-//        questionsArray[3].checkBox[1] = "Q 4 Answer 2";
-//        questionsArray[3].checkBox[2]  = "Q 4 answer 3";
-//        questionsArray[3].checkBox[3]  = "Q 4 answer 4";
-//        questionsArray[3].maxNumCheckbox = 4;
-//        questionsArray[3].answer = 2;
-//    }
+    public void setShortAnswerQuestions(){
 
+        questionsArray[0].questionHeader = "Short answer question 0";
+        questionsArray[1].questionHeader = "Short answer question 1";
+        questionsArray[2].questionHeader = "2";
+        questionsArray[3].questionHeader = "3";
+
+        questionsArray[0].checkBox[0]  = "Question 1 answer";
+        questionsArray[0].checkBox[1] = "Q 1 Answer 2";
+        questionsArray[0].checkBox[2]  = "Q 1 answer 3";
+        questionsArray[0].checkBox[3]  = "Q 1 answer 4";
+        questionsArray[0].maxNumCheckbox = 4;
+        questionsArray[0].answer = 2;
+
+        questionsArray[1].checkBox[0]  = "Question 2 answer";
+        questionsArray[1].checkBox[1] = "Q 2 Answer 2";
+        questionsArray[1].checkBox[2]  = "Q 2 answer 3";
+        questionsArray[1].checkBox[3]  = "Q 2 answer 4";
+        questionsArray[1].maxNumCheckbox = 4;
+        questionsArray[1].answer = 2;
+
+        questionsArray[2].checkBox[0]  = "Question 3 answer";
+        questionsArray[2].checkBox[1] = "Q 3 Answer 2";
+        questionsArray[2].checkBox[2]  = "Q 3 answer 3";
+        questionsArray[2].checkBox[3]  = "Q 3 answer 4";
+        questionsArray[2].maxNumCheckbox = 4;
+        questionsArray[2].answer = 2;
+
+        questionsArray[3].checkBox[0]  = "Question 4 answer";
+        questionsArray[3].checkBox[1] = "Q 4 Answer 2";
+        questionsArray[3].checkBox[2]  = "Q 4 answer 3";
+        questionsArray[3].checkBox[3]  = "Q 4 answer 4";
+        questionsArray[3].maxNumCheckbox = 4;
+        questionsArray[3].answer = 2;
+    }
+
+
+    public int getTotalNumberOfQuestions(){
+        return MAX_QUESTIONS;
+    }
+
+    public int getArrayIndex()
+    {
+        return arrayIndex;
+    }
+
+    public int getAnswer(){
+        return questionsArray[arrayIndex].answer;
+    }
 
     public String getEasyQuestion (){
             return questionsArray[arrayIndex].questionHeader;
